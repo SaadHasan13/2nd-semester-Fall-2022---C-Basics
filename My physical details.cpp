fc1e2db1ea3a313f978d9b066b16caef63626502
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2022-10-29 12:10:59 +0500

    Add files via upload

---
+++ My physical details.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+main(){
+	int age = 19;
+	char name[] = "Saad Hasan";
+	float height = 6.0;
+	char ShortName = 'S';
+	bool is_male = true;
+	
+	printf("Full name is: %s\n", name );
+	printf("Age is: %d\n", age);
+	printf("Height is: %f\n", height);
+	printf("Male?: %d\n", is_male);
+	getch();
+}
