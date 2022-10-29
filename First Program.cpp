144513e3b62106e81465ec1c1c0eec150c2bbd97
Saad Hasan <55943489+SaadHasan13@users.noreply.github.com>
2022-10-29 12:09:57 +0500

    Add files via upload

---
+++ First Program.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+# include <conio.h>
+main(){
+	printf("\nRoll Number is MG-026.");
+	printf("\nMy Name is Saad Hasan.");
+	printf("\nMy department is Management Sciences.");
+	getch();
+}
